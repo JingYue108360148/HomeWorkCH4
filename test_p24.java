b777a3e3782b035a70ca37ab2ea72be0ffad9e89
JingYue108360148 <t108360148@ntut.org.tw>
2021-12-09 14:30:00 +0800

    first

---
+++ test_p24.java
@@ -0,0 +1,32 @@
+package HW_04;
+
+public class test_p24 {
+    /*public static void main(String[] args){
+        Car car1 = new Car();
+        Car car2 = new Car();
+
+        Car car3;
+        car3 = car1;
+
+        System.out.println("car1與car2相同" + car1.equals(car2));
+        System.out.println("car1與car3相同" + car1.equals(car3));
+    }
+}
+class Car{
+    protected int num ;
+    protected double gas ;
+    public Car(){
+        num = 0 ;
+        gas = 0.0 ;
+        System.out.println("生產了車子");
+    }
+    /*public void setCar(int n, double g){
+        num = n ;
+        gas = g ;
+        System.out.println("將車號設為"+ num + "，汽油輛設為" + gas);
+    }
+    public String toString(){
+        String str ="車號:" + num + "，汽油量:" + gas;
+        return str;
+    }*/
+}
